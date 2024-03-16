686f4ee644ed467d4301961929d3cfc100746dad
Viacheslav Chugunov <viacheslav.chugunov@protonmail.com>
2024-03-16 14:18:39 +0200

    publishing update

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -24,11 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -45,7 +45,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.viacheslav-chugunov"
                 artifactId = "paged-effect"
-                version = "1.0.1"
+                version = "1.0.2"
             }
         }
     }
